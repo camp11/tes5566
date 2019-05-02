8cefa667e10b0d98a80921d36bd123944e251ada
camp11 <30381042+camp11@users.noreply.github.com>
2019-05-02 10:20:19 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1220,7 +1220,7 @@ if($message['type']=='text') {
     array (
       0 => 
       array (
-        'imageUrl' => 'https://stickershop.line-scdn.net/stickershop/v1/sticker/179170800/IOS/sticker_animation@2x.png;compress=true',
+        'imageUrl' => 'https://stickershop.line-scdn.net/stickershop/v1/sticker/179170800/IOS/sticker.png',
         'action' => 
         array (
           'type' => 'message',
