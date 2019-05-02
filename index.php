5422ab519d995fb99bc6c821a109bd6124699cb9
camp11 <30381042+camp11@users.noreply.github.com>
2019-05-02 10:13:54 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1205,6 +1205,37 @@ if($message['type']=='text') {
 }
 //pesan bergambar
 if($message['type']=='text') {
+	    if ($command == 'Haus' || $command == 'lapar' ) {
+
+        $balas = array(
+            'replyToken' => $replyToken,
+            'messages' => array(
+                array (
+  'type' => 'template',
+  'altText' => 'LOVE FIS mengirim sticker',
+  'template' => 
+  array (
+    'type' => 'image_carousel',
+    'columns' => 
+    array (
+      0 => 
+      array (
+        'imageUrl' => 'https://stickershop.line-scdn.net/stickershop/v1/sticker/179170800/IOS/sticker_animation@2x.png;compress=true',
+        'action' => 
+        array (
+          'type' => 'message',
+          'text' => 'Haus',
+        ),
+      ),
+    ),
+  ),
+)
+            )
+        );
+    }
+}
+//pesan bergambar
+if($message['type']=='text') {
 	    if ($command == 'logo' || $command == 'Logo' ) {
 
         $balas = array(
@@ -1219,7 +1250,6 @@ if($message['type']=='text') {
         );
     }
 }
-
 if (isset($balas)) {
     $result = json_encode($balas);
 //$result = ob_get_clean();
